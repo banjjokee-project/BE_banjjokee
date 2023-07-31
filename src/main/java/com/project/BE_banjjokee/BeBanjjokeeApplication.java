5ff19eb08ac0a8565e7e369bddb8c4f9faaac760
yeop0740 <ssucgoo@gmail.com>
2023-07-31 16:26:56 +0900

    기본 설정

---
+++ src/main/java/com/project/BE_banjjokee/BeBanjjokeeApplication.java
@@ -0,0 +1,13 @@
+package com.project.BE_banjjokee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeBanjjokeeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeBanjjokeeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/BE_banjjokee/BeBanjjokeeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.BE_banjjokee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeBanjjokeeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
