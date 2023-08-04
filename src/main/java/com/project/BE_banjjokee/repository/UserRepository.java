bbeff3dea35dd47622312de273133b89851b56c4
HyeongMin Kim <92067099+fkgnssla@users.noreply.github.com>
2023-08-04 11:16:30 +0900

    feat: OAuth2 구글 로그인 기능 추가

--- src/main/java/com/project/BE_banjjokee/repository/UserRepository.java
+++ src/main/java/com/project/BE_banjjokee/repository/UserRepository.java
@@ -1,5 +1,6 @@
 package com.project.BE_banjjokee.repository;
 
+import com.project.BE_banjjokee.model.SocialType;
 import com.project.BE_banjjokee.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
@@ -11,4 +12,6 @@ public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByNickname(String nickname);
 
     Optional<User> findByRefreshToken(String refreshToken);
+
+    Optional<User> findBySocialTypeAndSocialId(SocialType socialType, String socialId);
 }
