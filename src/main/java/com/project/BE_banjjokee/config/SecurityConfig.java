6804467deecef12de6289c3fe0c40115b4cc03fb
yeop0740 <ssucgoo@gmail.com>
2023-08-14 16:59:25 +0900

    chore: CORS 허용 설정

--- src/main/java/com/project/BE_banjjokee/config/SecurityConfig.java
+++ src/main/java/com/project/BE_banjjokee/config/SecurityConfig.java
@@ -42,6 +42,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
+                .cors(cors -> cors.disable())
                 .csrf(csrf -> csrf.disable())
                 .exceptionHandling(exceptionHandling -> exceptionHandling
                         .authenticationEntryPoint(new CustomAuthenticationEntryPoint())
