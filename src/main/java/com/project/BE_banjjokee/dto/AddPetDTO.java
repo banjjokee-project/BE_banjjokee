cfa339906432decb0543e8c1b94e1b481235da23
HyeongMin Kim <92067099+fkgnssla@users.noreply.github.com>
2023-08-21 15:28:32 +0900

    fix: Pet 생성, 수정 시 발생할 수 있는 예외상황 처리

--- src/main/java/com/project/BE_banjjokee/dto/AddPetDTO.java
+++ src/main/java/com/project/BE_banjjokee/dto/AddPetDTO.java
@@ -1,12 +1,14 @@
 package com.project.BE_banjjokee.dto;
 
 import com.project.BE_banjjokee.model.Pet;
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
 @AllArgsConstructor
 public class AddPetDTO {
+    @NotBlank
     String name;
     String imgUrl;
 
